f19b1d5c963672a90688bd511a323ec71ccc810d
Daniel Stoyanoff <dannystoyanoff@gmail.com>
2022-11-15 10:47:14 +0200

    test fix publish

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   organizationName: "dstoyanoff",
   projectName: "e-prim",
   deploymentBranch: "docs",
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
