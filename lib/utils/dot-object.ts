cb620363932ba20b032f5c690cff1178a4de61a3
Daniel Stoyanoff <dannystoyanoff@gmail.com>
2022-11-16 12:35:15 +0200

    [feature] Add support for numbers as keys of colors (#4)

--- lib/utils/dot-object.ts
+++ lib/utils/dot-object.ts
@@ -1,25 +1,35 @@
 export type PathsToStringProps<T, TStopType = string> = T extends TStopType
   ? []
   : {
-      [K in Extract<keyof T, string>]: [K, ...PathsToStringProps<T[K], TStopType>];
-    }[Extract<keyof T, string>];
+      [K in Extract<keyof T, string | number>]: [
+        K,
+        ...PathsToStringProps<T[K], TStopType>
+      ];
+    }[Extract<keyof T, string | number>];
 
-export type Join<T extends string[], D extends string = "."> = T extends []
+export type Join<
+  T extends (string | number)[],
+  D extends string = "."
+> = T extends []
   ? never
   : T extends [infer F]
   ? F
   : T extends [infer F, ...infer R]
-  ? F extends string
-    ? `${F}${D}${Join<Extract<R, string[]>, D>}`
+  ? F extends string | number
+    ? `${F}${D}${Join<Extract<R, (string | number)[]>, D>}`
     : never
-  : string;
+  : string | number;
 
 /**
  * Given a color key (dot-based deep path), resolves the actual color value
  * @param key dot-based path
  * @param object reference to the main object that should be used to find the value
  */
-export const getValueFromKey = <TValue, TKey extends string = string, TObject = {}>(
+export const getValueFromKey = <
+  TValue,
+  TKey extends string = string,
+  TObject = {}
+>(
   key: TKey | undefined,
   object: TObject
 ): TValue | undefined => {
